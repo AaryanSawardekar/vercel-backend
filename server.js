034e19abe410d38c845dc335afb33c819b679892
Aaryan Sawardekar <sawardekar.aaryan345@gmail.com>
2025-03-08 02:03:50 +0530

    idk what i fck

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "http://localhost:5173", 
+      "http://localhost:5173",
       "https://vercel-frontend-woad.vercel.app",
       "https://roast-my-stuff-hackathon.vercel.app"
     ],
